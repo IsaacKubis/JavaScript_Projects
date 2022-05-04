015adfc49b2668f2803af027bcc1c91d130cbc5e
IsaacKubis <104793931+IsaacKubis@users.noreply.github.com>
2022-05-04 12:19:12 -0700

    Add files via upload

---
+++ Project1_expressions_alert/Java/JavaScript_File.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+
+window.alert("Hello im an alert box!") // Alert box //
+
+var A = "This is a \"string\". "
+document.write(A)
+
+// concanetrate text //
+
+document.write("And this " 
+    + "is a concatenated "
+    + "line of text. ")
+
+// ----- //
+
+var math = ("The sum of 3+3 is ") // var math writen out //
+var math = math.fontcolor("green") // change font color of math //
+document.write(math, 3+3) // using the math var and an expressson
+
